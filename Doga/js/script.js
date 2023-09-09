32cbfa54d5312f1d4cb73b5ee7d934f93f4dae8b
viktor <komarov.v@triadasite.ru>
2023-09-09 20:23:01 +0300

    fix js bug

--- Doga/js/script.js
+++ Doga/js/script.js
@@ -710,20 +710,22 @@ if (tenderSearchInput && tenderSearchClear) {
     });
 }
 
-$(function(){
-    $(".trading-list").tradingList({
-        "domainUrl": "http://www.tender.pro",
-        "autoLoad": true,
-        "holding": false,
-        "transparent": false,
-        "params": {
-            "set_type_id": "1",
-            "types": "1, 5, 2, 4, 6, 3, 8, 7, 9",
-            "max_rows": "30",
-            "_key": "7b56c77b9f70220c3d5d4ce6477674ea",
-            "set_id": "381087"
-        }
+if (document.querySelector('.trading-list')) {
+    $(function(){
+        $(".trading-list").tradingList({
+            "domainUrl": "http://www.tender.pro",
+            "autoLoad": true,
+            "holding": false,
+            "transparent": false,
+            "params": {
+                "set_type_id": "1",
+                "types": "1, 5, 2, 4, 6, 3, 8, 7, 9",
+                "max_rows": "30",
+                "_key": "7b56c77b9f70220c3d5d4ce6477674ea",
+                "set_id": "381087"
+            }
+        });
     });
-});
+}
 
 ;

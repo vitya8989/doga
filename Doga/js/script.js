7558452cc5376c6431e30d0e8c2b00764ab4d220
viktor <komarov.v@triadasite.ru>
2023-09-27 04:19:51 +0300

    fix form focus

--- Doga/js/script.js
+++ Doga/js/script.js
@@ -246,41 +246,33 @@ if (uniqueSlider && uniqueTitleSlider) {
     });
 };
 const mainMap = document.querySelector('.main_map');
-const mainFullImg = document.querySelector('.main_full_img');
 const mainTopTitle = document.querySelector('.main_top__title');
 if (mainMap && mainTopTitle) {
     const mainMapContent = mainMap.querySelector('.js_main_map_content');
-    let topOfMainMapContent = mainMapContent.getBoundingClientRect().top;
+    const map1 = mainMap.querySelector('.js_map_1');
+    const map2 = mainMap.querySelector('.js_map_2');
+    const mapTitle1 = mainMap.querySelector('.js_map_title_1');
+    const mapTitle2 = mainMap.querySelector('.js_map_title_2');
 
-    let callback = function(entries, observer){
-        entries.forEach(entry => {
-            if (entry.target === mainFullImg) {
-                if (entry.isIntersecting) {
-                    if (topOfMainMapContent > entry.boundingClientRect.top && !mainMapContent.classList.contains('active_animation')) {
-                        disableScroll();
-                        mainMapContent.classList.add('active_animation');
-                        setTimeout(() => {
-                            enableScroll();
-                        }, 3700);
-                    }
-                } else {
-                    topOfMainMapContent = entry.boundingClientRect.top;
-                }
-            }
-            if (entry.target === mainTopTitle && entry.isIntersecting) {
-                if (mainMapContent.classList.contains('active_animation')) {
-                    mainMapContent.classList.remove('active_animation');
-                }
-            }
-        })
-    }
+    let scrollTop = mainMap.offsetTop;
+    let height = mainMap.offsetHeight;
+    let scrollBottom = height + scrollTop - mainMapContent.offsetHeight;
 
-    let observer = new IntersectionObserver(callback, {
-        threshold: 0.01,
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > scrollTop && window.pageYOffset < scrollBottom) {
+            let percent = ((window.pageYOffset - scrollTop) / (scrollBottom - scrollTop)).toFixed(2);
+            map1.style.opacity = (1 - (percent * 1.65)) > 0 ? 1 - (percent * 1.65) : 0;
+            map1.style.transform = `scale(${(8.5 * (percent * 2) + 1) > 9.5 ? 9.5 : (8.5 * (percent * 2) + 1) })`;
+
+            map2.style.opacity = (percent < 0.5 ? 0 : percent - (1.5 - percent * 2)) > 1 ? 1 : percent - (1.5 - percent * 2);
+
+            mapTitle1.style = `transform: translate(0, -${1500 * percent}px);`
+            mapTitle1.style.opacity = (1 - (percent * 1.85)) > 0 ? 1 - (percent * 1.85) : 0;
+
+            mapTitle2.style = `transform: translate(0, ${ (1500 - (1500 * percent * 1.3)) < 0 ? 0 : 1500 - (1500 * percent * 1.3)}px);`
+            mapTitle2.style.opacity = (percent < 0.5 ? 0 : percent - (1.5 - percent * 2)) > 1 ? 1 : percent - (1.5 - percent * 2);
+        }
     });
-
-    observer.observe(mainFullImg);
-    observer.observe(mainTopTitle);
 };
 const strategySlider = document.querySelector('.js_strategy_slider');
 
